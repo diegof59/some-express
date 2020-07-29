d069ffb0ef2633faf1e7447c014bfc38ce5767e5
diegof95 <diego.botero@correounivalle.edu.co>
2020-07-29 15:06:33 -0500

    Middleware example (logger). API now returns user with id (parameter from request).

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ app.get(
   }
 )
 
+app.get(
+  '/users/:id',
+  (request, response) => {
+    // users.users. get users array from users json
+    const result = users.users.filter((user) => (user.id === parseInt(request.params.id)))
+    console.log(users,' ',result)
+    if(result.length > 0){
+      response.json(result)
+    }
+    else{
+      response.status(400).json({msg: `User with id ${request.params.id} not found.`})
+    }
+  }
+)
+
 app.put(
   '/users',
   (request, response) => {
@@ -29,7 +44,15 @@ app.put(
   }
 )
 
+// Middleware example, a console logger
+function logger(request, response, next){
+
+  console.log(`Logger... Original Url: ${request.originalUrl}`)
+  // Executes next middleware
+  next()  
+}
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger) // Middleware activation
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
